d6ec1d6def7642cedf98744f6aa3072187c79da0
Tariq5mo <tariqomer756@gmail.com>
2024-08-03 19:38:13 +0300

    Add task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,29 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """
+        Returns a dictionary of models currently in storage.
+        or returns the list of objects of one type of class.
+        """
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            di = FileStorage.__objects
+            d = {}
+            for key in di:
+                class_name = eval(key.split(".")[0])
+                if cls == class_name:
+                    d[key] = di[key]
+            return d
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +44,30 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it's inside - if obj is equal to None
+        the method should not do anything.
+
+        Args:
+            obj (type, optional): instance of class. Defaults to None.
+        """
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        if obj is not None:
+            di = FileStorage.__objects
+            for key in di:
+                class_name = eval(key.split(".")[0])
+                id = key.split(".")[1]
+                if obj.__class__ == class_name and obj.id == id:
+                    del di[key]
+                    return
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -45,6 +89,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
